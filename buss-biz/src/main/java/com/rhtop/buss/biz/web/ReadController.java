6bd76c237ef45a1a9696ed2a7bcc7934c093dc07
dengli <dengli@rhtop.cn>
2017-01-04 15:49:24 +0800

    配置跨域支持，写注释定义接口命名方式。

--- buss-biz/src/main/java/com/rhtop/buss/biz/web/ReadController.java
+++ buss-biz/src/main/java/com/rhtop/buss/biz/web/ReadController.java
@@ -2,6 +2,7 @@ package com.rhtop.buss.biz.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rhtop.buss.biz.service.BusinessDiaryService;
@@ -11,8 +12,15 @@ import com.rhtop.buss.biz.service.CustomerService;
 import com.rhtop.buss.biz.service.RelCategoryPriceService;
 import com.rhtop.buss.biz.service.RelCustomerCategoryService;
 
+/**
+ * 对外接口的读取功能控制器，内部接口按照操作的表分为四类，操作客户相关表的命名为前缀Cu+四位编号0001依次递增，品类相关表为前缀Ca，交易相关表的为前缀Dl，合同相关表的为前缀Co.
+ * @author MakeItHappen
+ *
+ */
 @Controller
 @RequestMapping(value="/readData")
+//设置跨域支持
+@CrossOrigin(allowedHeaders="*")
 public class ReadController {
 	@Autowired
 	private CategoryService catSer;
--- buss-biz/src/main/java/com/rhtop/buss/biz/web/WriteController.java
+++ buss-biz/src/main/java/com/rhtop/buss/biz/web/WriteController.java
@@ -2,6 +2,7 @@ package com.rhtop.buss.biz.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.rhtop.buss.biz.service.BusinessDiaryService;
@@ -10,8 +11,15 @@ import com.rhtop.buss.biz.service.ContactsInfoService;
 import com.rhtop.buss.biz.service.CustomerService;
 import com.rhtop.buss.biz.service.RelCategoryPriceService;
 import com.rhtop.buss.biz.service.RelCustomerCategoryService;
+/**
+ * 对外接口的写入功能控制器，内部接口按照操作的表分为四类，操作客户相关表的命名为前缀Cu+四位编号0001依次递增，品类相关表为前缀Ca，交易相关表的为前缀Dl，合同相关表的为前缀Co.
+ * @author MakeItHappen
+ *
+ */
 @Controller
 @RequestMapping(value="/writeData")
+//配置跨域支持
+@CrossOrigin(allowedHeaders = "*")
 public class WriteController {
 	@Autowired
 	private CategoryService catSer;
