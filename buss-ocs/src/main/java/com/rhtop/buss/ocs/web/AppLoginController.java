caf5ad5f503d0005e8eb191880bf99a28892c8d1
maogaofei <maogaofei@rhtop.cn>
2017-01-04 15:51:38 +0800

    修改APP登录

--- buss-ocs/src/main/java/com/rhtop/buss/ocs/web/AppLoginController.java
+++ buss-ocs/src/main/java/com/rhtop/buss/ocs/web/AppLoginController.java
@@ -1,17 +1,23 @@
 package com.rhtop.buss.ocs.web;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import net.minidev.json.JSONObject;
+
 import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.rhtop.buss.common.entity.Member;
 import com.rhtop.buss.common.entity.User;
 import com.rhtop.buss.common.service.RestService;
 import com.rhtop.buss.common.utils.Jwt;
@@ -22,20 +28,29 @@ import com.rhtop.buss.common.web.BaseController;
 public class AppLoginController extends BaseController {
 	@Autowired
 	private RestService service;
+	
 	@RequestMapping("/login")
-	public String index(Model model,HttpServletRequest request){
-//		Subject subject = SecurityUtils.getSubject();
-//		User User = (User) subject.getPrincipals();
-//		//生成token
-//		Map<String , Object> payload=new HashMap<String, Object>();
-//		Date date=new Date();
-//		payload.put("uid", User.getUserId());//用户ID
-//		payload.put("iat", date.getTime());//生成时间
-//		payload.put("ext",date.getTime()+1000*60*60);//过期时间1小时
-//		String token=Jwt.createToken(payload);
-//		model.addAttribute("token",token);   
-		model.addAttribute("tree","");
-		return "index";
+	@ResponseBody
+	public Member login(HttpServletRequest request){
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		
+		UsernamePasswordToken token = new UsernamePasswordToken(userName, password);
+		SecurityUtils.getSubject().login(token);
+		
+		//生成token
+		Map<String , Object> payload=new HashMap<String, Object>();
+		Date date=new Date();
+		payload.put("uid", "admin");//用户ID
+		payload.put("iat", date.getTime());//生成时间
+		payload.put("ext",date.getTime()+1000*60*60);//过期时间1小时
+		String userToken=Jwt.createToken(payload);
+		
+		
+		
+//		model.addAttribute("tree","");
+//		return "index";
+		return null;
 	}
 	
 }
