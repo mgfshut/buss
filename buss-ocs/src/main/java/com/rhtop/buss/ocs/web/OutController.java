01b92a2586d25f2a879c8b2342f3c10abf5fd991
maogaofei <maogaofei@rhtop.cn>
2017-01-09 14:58:47 +0800

    修改

--- buss-ocs/src/main/java/com/rhtop/buss/ocs/web/OutController.java
+++ buss-ocs/src/main/java/com/rhtop/buss/ocs/web/OutController.java
@@ -12,7 +12,6 @@ import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
