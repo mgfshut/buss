db4a56fe7d47f036ce96e6844f9a8deec0e92920
鲁津 <lujn@rhtop.cn>
2017-01-13 08:50:48 +0800

    定时任务：品类价格每周末凌晨一点清除。

---
+++ buss-biz/src/main/java/com/rhtop/buss/biz/service/HisRelCategoryPriceService.java
@@ -0,0 +1,16 @@
+/**
+ * 代码声明
+ */
+package com.rhtop.buss.biz.service;
+
+import com.rhtop.buss.common.entity.RelCategoryPrice;
+
+public interface HisRelCategoryPriceService{
+    
+	/**
+	 * 复制RelCategoryPrice表
+	 */
+	int insertRelCategoryPrice(RelCategoryPrice relCategoryPrice);
+    
+	
+}
