a265d5d66e17843347f7dcdb9e3469b00e6ed722
libinbin <libinbin@rhtop.cn>
2017-01-20 13:02:12 +0800

    更新品类信息

--- buss-biz/src/main/java/com/rhtop/buss/biz/web/CategoryController.java
+++ buss-biz/src/main/java/com/rhtop/buss/biz/web/CategoryController.java
@@ -84,7 +84,12 @@ public class CategoryController  extends BaseController {
 				catPriSer.insertRelCategoryPrice(relCategoryPrice);	 
 				htmlMessage.setMessage("添加成功！");
 			 }else{//存在
-				 //如果存在
+				//如果存在
+				cate.setOfferPri(category.getOfferPri());
+				cate.setOfferAging(category.getOfferAging());
+				cate.setUpdateUser(userId);
+				cate.setUpdateTime(DateUtils.getToday("yyyy-MM-dd HH:mm:ss"));
+				categoryService.updateCategory(cate);
 				//将供应商，货币单位，计量单位，报价，时效,品类主键加入到品类与价格关系表中
 				RelCategoryPrice relCategoryPrice = new RelCategoryPrice();
 				relCategoryPrice.setRelCategoryPriceId(UUID.randomUUID().toString().replace("-", ""));
