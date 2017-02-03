2c0a53a5728011480ce430a370dfd25857f62187
maogaofei <maogaofei@rhtop.cn>
2017-02-03 16:17:22 +0800

    修改品类导出相关sql

--- buss-biz/src/main/java/com/rhtop/buss/biz/quartz/ClearPriceJob.java
+++ buss-biz/src/main/java/com/rhtop/buss/biz/quartz/ClearPriceJob.java
@@ -39,7 +39,12 @@ public class ClearPriceJob  extends QuartzJobBean{
 		List<RelCategoryPrice>  rels = relCategoryPriceService.listRelCategoryPrices(new RelCategoryPrice(), null);
 		for(RelCategoryPrice rel:rels){
 			//复制记录
-			hisRelcategoryPriceService.insertRelCategoryPrice(rel);
+			try {
+				hisRelcategoryPriceService.insertRelCategoryPrice(rel);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println(rel.getCategoryId());
 		}
 		//删除主表记录
